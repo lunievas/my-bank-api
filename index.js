5127f204d6df44dc69a16818aa0627935128d6e4
lunievas <luisa.nievas94@gmail.com>
2020-08-03 20:53:52 -0300

    verificando se já existe um arquivo accounts, se não existir ele cria o arquivo com writeFile e readFile

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from 'express';
-import accountsRouter from './routes/accounts.js'
+import accountsRouter from './routes/accounts.js';
+import {promises as fs} from 'fs';
+
+const {readFile, writeFile} = fs;
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log('API started :D')
+app.use('/account', accountsRouter);
+
+app.listen(3000, async ()=>{
+    try{
+        await readFile('accounts.json');
+        console.log('API started :D')
+    }catch(err){
+        const initialJson = {
+            nextId: 1,
+            accounts:[]
+        }
+        writeFile('accounts.json', JSON.stringify(initialJson)).then(()=>{
+
+            console.log('API started and file created :D');
+        }).catch(err =>{
+            console.log(err);
+        });
+    }
+
 });
