9ae18fee87cb3f92cc5d7580666b3a9e2c4c3737
lunievas <luisa.nievas94@gmail.com>
2020-08-03 20:36:54 -0300

    criando uma pasta para rotas, criando uma rota para accounts, exportando default e importando no index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import accountsRouter from './routes/accounts.js'
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, ()=>{
-
+    console.log('API started :D')
 });
