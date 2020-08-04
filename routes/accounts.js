f917b795aa5af5f77df358d83b87fb66b658a921
lunievas <luisa.nievas94@gmail.com>
2020-08-04 19:48:28 -0300

    criando a validação dos campos

--- routes/accounts.js
+++ routes/accounts.js
@@ -8,9 +8,18 @@ const router = express.Router();
 router.post('/', async (req, res,next)=>{
     try{
         let account = req.body;
+
+        if(!account.name || account.balance == null){
+            throw new Error('Name e Balance são obrigatórios')
+        }
+
+
         const data = JSON.parse(await readFile('accounts.json'));
 
-        account = {id: data.nextId++, ...account};
+        account = {id: data.nextId++,
+                   name:account.name,
+                   balance: account.balance
+                };
         data.accounts.push(account);
 
         await writeFile(global.fileName, JSON.stringify(data, null,2));
@@ -64,12 +73,23 @@ router.delete('/:id', async (req, res, next)=>{
 router.put('/', async (req,res, next)=>{
     try{
         const account = req.body;
+
+        if(!account.id ||!account.name || account.balance == null){
+            throw new Error('Id, Name e Balance são obrigatórios')
+        }
+
+
         const data = JSON.parse(await readFile(global.fileName));
         const index = data.accounts.findIndex(a => a.id === account.id );
 
-        data.accounts[index] = account;
+        if(index === -1){
+            throw new Error('Registro não encontrado.')
+        }
+        data.accounts[index].name = account.name;
+        data.accounts[index].balance = account.balance;
+        
 
-        await writeFile(global.fileName, JSON.stringify(data));
+        await writeFile(global.fileName, JSON.stringify(data, null,2));
 
         res.send(account);
         logger.info(`PUT /account - ${JSON.stringify(account)}`);
@@ -84,9 +104,18 @@ router.patch('/updateBalance',async(req,res, next)=>{
         const data = JSON.parse(await readFile(global.fileName));
         const index = data.accounts.findIndex(a => a.id === account.id );
 
+        if(!account.id|| account.balance == null){
+            throw new Error('Id e Balance são obrigatórios')
+        }
+
+
+        if(index === -1){
+            throw new Error('Registro não encontrado.')
+        }
+
         data.accounts[index].balance = account.balance;
 
-        await writeFile(global.fileName, JSON.stringify(data));
+        await writeFile(global.fileName, JSON.stringify(data,null, 2));
 
         res.send(data.accounts[index]);
         logger.info(`PATCH /account/updateBalance - ${JSON.stringify(account)}`);
