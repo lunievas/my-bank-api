4b9f23e5e6f5191c51a2d57b72b0b6b2ac32b091
lunievas <luisa.nievas94@gmail.com>
2020-08-03 22:16:21 -0300

    Criando o método GET que faz a leitura do json e mostra para o usuário

--- routes/accounts.js
+++ routes/accounts.js
@@ -13,7 +13,7 @@ router.post('/', async (req, res)=>{
         account = {id: data.nextId++, ...account};
         data.accounts.push(account);
 
-        await writeFile('accounts.json', JSON.stringify(data, null,2));
+        await writeFile(global.fileName, JSON.stringify(data, null,2));
 
         
 
@@ -24,5 +24,15 @@ router.post('/', async (req, res)=>{
 
 });
 
+router.get('/', async (req,res)=>{
+    try{
+        
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data);
+    }catch(err){
+        res.status(400).send({error: err.message});
+    }
+});
 
 export default router;
